e7306fd25b2a676c9be6d69b50aa7d23bf888fef
Mohd Wajid <88840217+mohdWajid19@users.noreply.github.com>
2021-12-02 19:41:11 +0530

    Update main.py

--- Higher_lower_game/main.py
+++ Higher_lower_game/main.py
@@ -11,7 +11,7 @@ def print_opponent(opponent, sno):
     '''prints the opponent's data'''
     print(f"\n competant : {sno}", end=". ")
     print(opponent['name'],end = ', ')
-    # print(opponent['follower_count'],end = ', ')
+    print(opponent['follower_count'],end = ', ')
     print(opponent['description'],end = ', ')
     print(opponent['country'],end = "\n\n")
 
@@ -24,9 +24,9 @@ def compare_opponents(opponent1, opponent2):
         return 0
 
 def user_voted(string):
-    if string == 'higher':
+    if string == '1':
         return 1
-    elif string == 'lower':
+    elif string == '2':
         return 2
     else:
         return 0
@@ -35,20 +35,21 @@ def play_game():
     '''the actual game engine of higher or lower game'''
     score = 0
     is_correct = True
+    opponent_1 = give_opponent()
     while is_correct:
         clear()
-        print(f'score = {score}')
         print(logo)
-        opponent_1 = give_opponent()
+        print(f'You got it correct, score = {score}')
         print_opponent(opponent_1 ,1)
         print(vs)
         opponent_2 = give_opponent()
         print_opponent(opponent_2,2)
-        user_guess = input("GUESS WHICH COMPETANT HAS GOT MORE FOLLOWERS HIGHER ONE OR LOWER ONE \n (Enter higher or lower) :: ").lower()
+        user_guess = input("GUESS WHICH COMPETANT HAS GOT MORE FOLLOWERS HIGHER ONE OR LOWER ONE \n (Enter 1 or 2) :: ")
         user_guessed = user_voted(user_guess)
         more_famous = compare_opponents(opponent_1,opponent_2)
         if user_guessed != 0 and user_guessed == more_famous:
             score += 1
+            opponent_1 = opponent_2
         elif more_famous == 0:
             score += 1
             print("You got lucky, they both got same followers")
@@ -59,8 +60,6 @@ def play_game():
             is_correct = False
     if input("wanna play again (y/n): ") == 'y':
         play_game()
-    
-
 
 
 play_game()
